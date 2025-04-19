8bfe82d174234d511ff449599d67b065b613e1bd
Jahon A <amjahongir@gmail.com>
2025-04-19 15:04:23 -0700

    Add code

--- local_lambda_function.py
+++ local_lambda_function.py
@@ -10,10 +10,10 @@ POST_EVENT = {
 
 GET_EVENT = {
     "resource": "/{short_code}",
-    "path": "/d1a766",
+    "path": "/d0e196",
     "httpMethod": "GET",
     "pathParameters": {
-        "short_code": "d1a766"
+        "short_code": "d0e196"
     },
 }
 
@@ -33,8 +33,18 @@ def lambda_handler(event, context):
     # if http method is POST
     #   generate the short code from the original URL
     #   return the short code for the original URL
-    pass
+    if event['httpMethod'] == 'GET':
+        event_parameters = event.get('pathParameters', {})
+        short_code = event_parameters.get('short_code')
+        return DATABASE.get(short_code)
+    elif event['httpMethod'] == 'POST':
+        body = json.loads(event.get('body', "{}"))
+        url = body.get('url', '')
+        generated_short_url = generate_short_key(url)
+        DATABASE[generated_short_url] = url
+        return generated_short_url
 
 if __name__ == "__main__":
     print(lambda_handler(POST_EVENT, {}))
     print(lambda_handler(GET_EVENT, {}))
+    print(DATABASE)
